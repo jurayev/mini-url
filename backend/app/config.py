5954c15d9e5be8e214378646b2462169aefc735b
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2021-04-21 00:26:36 +0300

    Adds updated config vars for prod usage

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
     REDIS_HOST: str = os.getenv("REDIS_HOST", "0.0.0.0")
     REDIS_PORT: int = os.getenv("REDIS_PORT", 6379)
     REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD", "")
+    MINI_URL: str = os.getenv("MINI_URL", f"{APP_PROTOCOL}://{APP_HOST}:{APP_PORT}")
     REDIS_URL: str = os.getenv("REDIS_URL", f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0")
 
 
