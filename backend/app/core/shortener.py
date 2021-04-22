d8cc4b0ac9028541101159ea4e9ec3e447495514
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2021-04-22 17:14:42 +0300

    Refactor shortener class: increase retries, remove logging, key name updates

--- backend/app/core/shortener.py
+++ backend/app/core/shortener.py
@@ -9,12 +9,11 @@ class Shortener:
         self.mapping = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         self.db = db
         self.url_length = 8
-        self.retries = 10
-        self.key_prefix = "shortener:"
+        self.retries = 15
+        self.key_prefix = "key:"
 
     def short_to_long(self, short_key: str) -> Any:
-        # store the key as "short_key:aBd3Xa30"
-        #print("REdis:", self.db.keys("*"))
+        # store the key as "key:aBd3Xa30"
         full_key = self.key_prefix + short_key
         long_url = self.db.get(full_key)
         if not long_url:
@@ -23,13 +22,11 @@ class Shortener:
         return long_url
 
     def long_to_short(self, url: str) -> Any:
-        #print("Instance Name:", self.db.client_id())
         for i in range(0, self.retries):
             short_key = self.generate_key()
             full_key = self.key_prefix + short_key
             response = self.db.set(full_key, url, nx=True)
             if response is not None:
-                #print("REdis:", self.db.keys("*"))
                 return short_key
         raise KeyError(f"Tried to create a short key {self.retries} times. Please try again!")
 
